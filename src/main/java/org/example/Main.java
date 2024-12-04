b7e795868015ae61b0a594469f92cab4ff125506
Darcy <110570447+demccusker@users.noreply.github.com>
2024-12-04 14:14:14 -0800

    Day 2, part 1 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,8 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        day1();
+        //day1();
+        day2();
 
     }
 
@@ -59,5 +60,49 @@ public class Main {
 
 
     }
+    public static void day2(){
+        List<List<Integer>> reports = new ArrayList<>();
+        int count = 0;
+        try{
+            File input = new File("C:\\Users\\Darcy\\IdeaProjects\\adventofcode\\src\\main\\java\\org\\example\\adventofcodeday2input.txt");
+            Scanner scanner = new Scanner(input);
 
+            while (scanner.hasNextLine()){
+                List<Integer> levels = new ArrayList<>();
+                String line = scanner.nextLine();
+                String[] numbers = line.split("\\s+");
+                for ( String num : numbers){
+                    levels.add(Integer.parseInt(num));
+                }
+                reports.add(levels);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: Input file not found.");
+        }
+        for (List<Integer> level : reports){
+            boolean alwaysIncreasing = true;
+            boolean alwaysDecreasing = true;
+            boolean noGapsGreaterThanThree = true;
+            for (int i = 1; i < level.size(); i++) {
+                if ((Math.abs(level.get(i -1) - level.get(i)) > 3) || (Math.abs(level.get(i - 1) - level.get(i)) < 1)){
+                    noGapsGreaterThanThree = false;
+                    System.out.println(level);
+                }
+                if (level.get(i - 1) < level.get(i)){
+                    alwaysDecreasing = false;
+                }
+                if (level.get(i -1) > level.get(i)){
+                    alwaysIncreasing = false;
+                }
+
+            }
+            if (noGapsGreaterThanThree && (alwaysDecreasing || alwaysIncreasing)){
+                //System.out.println(level);
+                count+=1;
+            }
+        }
+        System.out.println("count of safe reports: " + count);
+    }
 }
